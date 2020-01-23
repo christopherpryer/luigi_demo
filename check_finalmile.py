5f25455dc29ecda092ba89ceb8f181797bf279e6
christopherpryer <christophpryer@gmail.com>
2020-01-23 14:41:03 +0000

    added base for custom luigi task

---
+++ check_finalmile.py
@@ -0,0 +1,4 @@
+from tmp.shipments import df
+
+if __name__ == '__main__':
+    print(df.shape)
---
+++ top_opportunities.py
@@ -0,0 +1,6 @@
+"""
+Example script to manage luigi tasks for opportunity processing 
+on shipment data. An opportunity is defined as a positive change
+or further leverageing of element(s) in the existing operation,
+as seen in the data.
+"""
